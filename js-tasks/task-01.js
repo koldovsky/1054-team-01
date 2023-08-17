506fd84ae330557d1f5cabffbd476228ff31c35b
Mariia <133953467+NovitskaMariia@users.noreply.github.com>
2023-08-17 20:08:33 +0300

    Update task-01.js

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -1,45 +1,6 @@
 // Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
-// Slava 
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-}
-
-//Mariia
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   const total = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
   return total
 }
-
-//Olesia
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-}
-
-//OstapAV
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  const goals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-  return goals;
-}
-
-//Yuliia
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-}
-
-<<<<<<< HEAD
-//Lesia
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  const sumGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-  return sumGoals;
-}
-=======
-//Vatrych
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  const goals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-  return goals;
-}
-//Dmytro
-function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
->>>>>>> 0d56a388409522ede026900eab969d1e7870e9b9
