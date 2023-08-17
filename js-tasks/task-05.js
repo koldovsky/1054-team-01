9f842263c41a26898695b565b46c6c7c1b7e7171
Mariia <133953467+NovitskaMariia@users.noreply.github.com>
2023-08-17 20:24:58 +0300

    Update task-05.js

--- js-tasks/task-05.js
+++ js-tasks/task-05.js
@@ -1,45 +1,5 @@
 // Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
-// Slava 
-function litres(time) {
-  return Math.floor(time * 0.5);
-}
-
-// Mariia
-function litres(time) {
-  return Math.floor(time * 0.5);
-}
-
-//Olesia
 function litres(time) {
   return (time / 2) | 0;
 }
-
-//OstapAV
-function litres(time) {
-  const litresPerHour = 0.5;
-  const litres = Math.floor(time * litresPerHour);
-  return litres;
-  return (time / 2) | 0;
-}
-
-//Yuliia
-function litres(time) {
-<<<<<<< HEAD
-    return Math.floor(time * 0.5);
-}
-
-//Lesia
-function litres(time) {
-  const liter = parseInt(time * 0.5);
-  return liter;
-}
-=======
-  return Math.floor(time * 0.5);
-}
-
-//Vatrych
-function litres(time) {
-   return Math.floor(time * 0.5);
-}
->>>>>>> 0d56a388409522ede026900eab969d1e7870e9b9
