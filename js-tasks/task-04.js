f5c2128786ffcfbe876a656a36b71c9ac13282a3
Mariia <133953467+NovitskaMariia@users.noreply.github.com>
2023-08-17 20:21:18 +0300

    Update task-04.js

--- js-tasks/task-04.js
+++ js-tasks/task-04.js
@@ -1,64 +1,8 @@
 // Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
-// Slava
-function greet(name, owner) {
-  if (name === owner) {
-    return "Hello boss";
-  } else {
-    return "Hello guest";
-  }
-}
-
-//Mariia
-function greet(name, owner) {
-  if (name === owner) {
-    return "Hello boss";
-  } else {
-    return "Hello guest";
-  }
-}
-
-//Olesia
 function greet(name, owner) {
   if (name === owner) {
     return 'Hello boss';
   }
   return 'Hello guest';
 }
-
-//OstapAV
-function greet(name, owner) {
-  if (name === owner) {
-    return 'Hello boss';
-  } else {
-    return 'Hello guest';
-  }
-}
-
-//Yuliia
-function greet(name, owner) {
-  if (name === owner) {
-    return "Hello boss";
-  }
-  return "Hello guest";
-}
-
-<<<<<<< HEAD
-//Lesia
-function greet(name, owner) {
-  if (name === owner) {
-    return 'Hello boss'; 
-  } else {
-    return 'Hello guest';
-  }
-}
-=======
-//Vatrych
-function greet (name, owner) {
-  if (name === owner);
-  return 'Hello boss';
-  else {
-    return 'Hello guest';
-  }
-}
->>>>>>> 0d56a388409522ede026900eab969d1e7870e9b9
