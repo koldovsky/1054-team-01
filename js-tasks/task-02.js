5771fc5c3b4fd2d38bb22626304bf3034e943c37
Mariia <133953467+NovitskaMariia@users.noreply.github.com>
2023-08-17 20:18:35 +0300

    Update task-02.js

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -1,64 +1,8 @@
 // Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
-// Slava
-function makeNegative(num) {
-  if (num < 0) {
-    return num;
-  } else {
-    return -num;
-  }
-}
-
-//Mariia
-function makeNegative(num) {
-  if (num <= 0) {
-    return num
-  } else {
-    return -1 * num
-  }
-}
-
-//Olesia
-function makeNegative(num) {
-  if (num < 0) {
-    return num;
-  }
-  return -num;
-}
-
-//OstapAV
-function makeNegative(num) {
-  if (num > 0) {
-    return -num;
-  }
-}
-
-//Yuliia
 function makeNegative(num) {
   if (num > 0) {
     return -num;
   }
   return num;
 }
-
-<<<<<<< HEAD
-//Lesia
-function makeNegative(num) {
-  if (num === 0) {
-    return 0;
-  } else if (num > 0) {
-    return -num;
-  } else {
-    return num;
-  }
-}
-=======
-//Vatrych
-function makeNegative(num) {
- return -Math.abs(num);
-} 
-
-function makeNegative(num) {
-  return -Math.abs(num);
-} 
->>>>>>> 0d56a388409522ede026900eab969d1e7870e9b9
