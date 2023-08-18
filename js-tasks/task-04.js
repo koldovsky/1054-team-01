212c23cba8a5a51bc86991aff6ccb4ae328f1991
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-08-18 21:36:47 +0300

    Update task-04.js

--- js-tasks/task-04.js
+++ js-tasks/task-04.js
@@ -1,8 +1,1 @@
-// Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
-function greet(name, owner) {
-  if (name === owner) {
-    return 'Hello boss';
-  }
-  return 'Hello guest';
-}
