098aac3ad52513ca62abca5fcb4c556e71a4f130
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-08-18 21:36:06 +0300

    Update task-02.js

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -1,8 +1,28 @@
-// Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
-function makeNegative(num) {
-  if (num > 0) {
-    return -num;
-  }
-  return num;
-}
+
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+// Yuliia
+var min = function (list) {
+  return Math.min(...list);
+};
+
+var max = function (list) {
+  return Math.max(...list);
+};
+
+
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+
+// https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+
+
+// https://www.codewars.com/kata/beginner-lost-without-a-map
+
+
+// https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
