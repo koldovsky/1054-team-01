ce81d4fd57e090402544c62fc82d17daa35124ed
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-08-19 15:35:42 +0300

    Update task-02.js

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -8,6 +8,10 @@ function stringToArray(string) {
 
 // https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 
+// Yuliia
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, "U");
+}
 
 // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
