2bbefad6972ca2cb63ba02a16b34c4b7cd503ce2
HolodLesia <45822753+HolodLesia@users.noreply.github.com>
2023-08-19 18:06:35 +0300

    Solution of tasks in js is added

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -48,3 +48,53 @@ function min(arr, toReturn) {
 
 
 // https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
+
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+// Lesia
+function stringToArray(string){
+  return string.split(' ')
+}
+
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+// Lesia
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, 'U');
+}
+
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+// Lesia
+const min = function(list) {
+  let minValue = list[0];
+  for (let i = 1; i < list.length; i++) {
+      if (list[i] < minValue) {
+          minValue = list[i];
+      }
+  }
+  return minValue;
+};
+const max = function(list) {
+  let maxValue = list[0];     
+  for (let i = 1; i < list.length; i++) {
+      if (list[i] > maxValue) {
+          maxValue = list[i];
+      }
+  }
+  return maxValue;
+};
+
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+//Lesia
+function min(arr, toReturn) {
+  if (toReturn === 'value') {
+      const minValue = Math.min(...arr);
+      return minValue;
+  } else if (toReturn === 'index') {
+      const minIndex = arr.indexOf(Math.min(...arr));
+      return minIndex;
+  }
+}
+
