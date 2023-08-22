bf25982526a7749b8e24941bec670133aaadc92f
Mariia Novytska <mnovickaa@gmail.com>
2023-08-22 16:42:30 +0300

    Added last 2 tasks from homework

--- js-tasks/task-03.js
+++ js-tasks/task-03.js
@@ -31,7 +31,47 @@ function giveMeFive(obj) {
 
 // =============================================================================================================
 // https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+//Mariia
+function buildFun(n){
 
+	const res = []
+
+	for (let i = 0; i< n; i++){
+		res.push(function(){
+ 			console.log(i)
+      return i
+		})
+	}
+	return res
+}
 
 // =============================================================================================================
 // https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+//Mariia
+// Get Coding :)
+
+class Shark extends Animal {
+    constructor(name, age, status) {
+      super(name, age, 0, "shark", status);
+    }
+  }
+  
+  class Cat extends Animal {
+    constructor(name, age, status) {
+      super(name, age, 4, "cat", status);
+  }
+    
+    introduce() {
+      return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name, age, status, master) {
+      super(name, age, 4, "dog", status);
+      this.master = master;
+    }
+    greetMaster() {
+      return `Hello ${this.master}`
+    }
+  }
