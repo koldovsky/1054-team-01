90441840ac308efcf3fa105cf100b5f2ffff3ee8
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-08-22 11:20:30 +0300

    Update task-03.js

--- js-tasks/task-03.js
+++ js-tasks/task-03.js
@@ -1,1 +1,17 @@
-//task 3
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+
+// ============================================================================================================
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
+
+// =============================================================================================================
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+
+// =============================================================================================================
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+
+// =============================================================================================================
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
