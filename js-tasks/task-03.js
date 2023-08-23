702bc8fa770f39f11659bf3ecc6e240c3189ba13
Mariia <133953467+NovitskaMariia@users.noreply.github.com>
2023-08-23 15:53:47 +0300

    Update task-03.js formating

--- js-tasks/task-03.js
+++ js-tasks/task-03.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
 class SmallestIntegerFinder {
   findSmallestInt(args) {
-    return Math.min(...args)
+    return Math.min(...args);
   }
 }
 
@@ -14,48 +14,48 @@ function circleCircumference(circle) {
 // =============================================================================================================
 // https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
 function giveMeFive(obj) {
-  const arr = []
+  const arr = [];
   for (const key in obj) {
-    if (key.length === 5) arr.push(key)
-    if (obj[key].length === 5) arr.push(obj[key])
+    if (key.length === 5) arr.push(key);
+    if (obj[key].length === 5) arr.push(obj[key]);
   }
-  return arr
+  return arr;
 }
 
 // =============================================================================================================
 // https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
-function buildFun(n){
-	const res = []
-	for (let i = 0; i < n; i++){
-		res.push(() => i)
-	}
-	return res
+function buildFun(n) {
+  const res = [];
+    for (let i = 0; i < n; i++) {
+      res.push(() => i);
+    }
+  return res;
 }
 
 // =============================================================================================================
 // https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
 class Shark extends Animal {
   constructor(name, age, status) {
-    super(name, age, 0, 'shark', status)
+    super(name, age, 0, 'shark', status);
   }
 }
 
 class Cat extends Animal {
   constructor(name, age, status) {
-    super(name, age, 4, 'cat', status)
+    super(name, age, 4, 'cat', status);
   }
 
   introduce() {
-    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
   }
 }
 
 class Dog extends Animal {
   constructor(name, age, status, master) {
-    super(name, age, 4, 'dog', status)
-    this.master = master
+    super(name, age, 4, 'dog', status);
+    this.master = master;
   }
   greetMaster() {
-    return `Hello ${this.master}`
+    return `Hello ${this.master}`;
   }
 }
