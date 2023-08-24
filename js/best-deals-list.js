0e76622e1b6593693710d97dae60c4fa9d385955
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-08-24 15:02:28 +0000

    Finished best deals carousel

--- js/best-deals-list.js
+++ js/best-deals-list.js
@@ -10,12 +10,12 @@ prevButton.addEventListener('click', prevSlide)
 renderProduct(catalogue.products)
 
 function renderProduct(products) {
-  const productsContainer = document.querySelector('.best-deals__products')
-  productsContainer.innerHTML = ''
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].bestDeal) {
-      const content = 
-        `<div class="best-deals__product product">
+    const productsContainer = document.querySelector('.best-deals__products')
+    productsContainer.innerHTML = ''
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].bestDeal) {
+            const content =
+                `<div class="best-deals__product product">
             <div class="best-deals__product product">
                 <a href="store-product.html"><img src="${products[i].images[0]}" alt="${products[i].name}"></img></a>
                 <a href="store-product.html" class="product__name">${products[i].name}</a>
@@ -23,37 +23,35 @@ function renderProduct(products) {
                 <a class="button" href="#"><strong>Add to cart</strong></a>
             </div>
         </div>`
-        // productsContainer.innerHTML += content
-        slides.push(content)
+            slides.push(content)
+        }
     }
-  }
-  renderSlide()
+    renderSlide()
 }
 
 function renderSlide() {
-  const productsContainer = document.querySelector('.best-deals__products')
-  productsContainer.innerHTML = ''
-  productsContainer.innerHTML += slides[currentSlide]
-  if (window.matchMedia('(min-height: 242px)').matches) {
-    const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1
-    productsContainer.innerHTML += slides[secondSlide]
-    if (window.matchMedia('(min-width: 1024px)').matches) {
-      const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1
-      productsContainer.innerHTML += slides[thirdSlide]
+    const productsContainer = document.querySelector('.best-deals__products')
+    productsContainer.innerHTML = ''
+    productsContainer.innerHTML += slides[currentSlide]
+    if (window.matchMedia('(min-height: 242px)').matches) {
+        const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1
+        productsContainer.innerHTML += slides[secondSlide]
+        if (window.matchMedia('(min-width: 1024px)').matches) {
+            const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1
+            productsContainer.innerHTML += slides[thirdSlide]
+        }
     }
-  }
 }
 
-
 function nextSlide() {
-  currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1
-  renderSlide()
+    currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1
+    renderSlide()
 }
 
 
 function prevSlide() {
-  currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1
-  renderSlide()
+    currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1
+    renderSlide()
 }
 
 
