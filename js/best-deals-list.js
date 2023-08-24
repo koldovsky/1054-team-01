243a8cc807b43683cb65a81fbd06701aedc27cbe
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-08-24 14:03:24 +0000

    Fixed mistakes in carousel

--- js/best-deals-list.js
+++ js/best-deals-list.js
@@ -1,23 +1,60 @@
 import * as catalogue from './products-catalogue.js'
 
+const slides = []
+let currentSlide = 0
+const nextButton = document.querySelector('.best-deals__arrowL')
+nextButton.addEventListener('click', nextSlide)
+const prevButton = document.querySelector('.best-deals__arrowR')
+prevButton.addEventListener('click', prevSlide)
 
-renderProduct(catalogue.products);
+renderProduct(catalogue.products)
 
 function renderProduct(products) {
-    const productsContainer = document.querySelector('.best-deals__products');
-    productsContainer.innerHTML = '';
-    for (const product of products) {
-        if (product.bestDeal) {
-            productsContainer.innerHTML +=
-                `<div class="best-deals__product product">
-                <div class="best-deals__product product">
-                    <a href="store-product.html"><img src="${product.images[0]}" alt="${product.name}"></img></a>
-                    <a href="store-product.html" class="product__name">${product.name}</a>
-                    <p class="product__price">${product.price.toFixed(2)}${product.currency}</p>
-                    <a class="button" href="#"><strong>Add to cart</strong></a>
-                </div>
-            </div>`;
-        }
+  const productsContainer = document.querySelector('.best-deals__products')
+  productsContainer.innerHTML = ''
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].bestDeal) {
+      const content = 
+        `<div class="best-deals__product product">
+            <div class="best-deals__product product">
+                <a href="store-product.html"><img src="${products[i].images[0]}" alt="${products[i].name}"></img></a>
+                <a href="store-product.html" class="product__name">${products[i].name}</a>
+                <p class="product__price">${products[i].price.toFixed(2)}${products[i].currency}</p>
+                <a class="button" href="#"><strong>Add to cart</strong></a>
+            </div>
+        </div>`
+        // productsContainer.innerHTML += content
+        slides.push(content)
     }
+  }
+  renderSlide()
 }
 
+function renderSlide() {
+  const productsContainer = document.querySelector('.best-deals__products')
+  productsContainer.innerHTML = ''
+  productsContainer.innerHTML += slides[currentSlide]
+  if (window.matchMedia('(min-height: 242px)').matches) {
+    const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1
+    productsContainer.innerHTML += slides[secondSlide]
+    if (window.matchMedia('(min-width: 1024px)').matches) {
+      const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1
+      productsContainer.innerHTML += slides[thirdSlide]
+    }
+  }
+}
+
+
+function nextSlide() {
+  currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1
+  renderSlide()
+}
+
+
+function prevSlide() {
+  currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1
+  renderSlide()
+}
+
+
+window.addEventListener('resize', renderProduct)
--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 document.addEventListener('partialsLoaded', () => {
-    import('./best-deals-carousel.js');
     import('./best-deals-list.js');
     import('./shop-list.js');
     import('./contact-form.js');
