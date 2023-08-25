97fcf027206ebc1a1f4bab2647729ab8d61f4ffc
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-08-25 07:59:45 +0000

    Fixed mistakes in view all button

--- js/shop-list.js
+++ js/shop-list.js
@@ -1,20 +1,56 @@
 import * as catalogue from './products-catalogue.js'
 
+const slides = []
+let currentSlide = 0
+const nextButton = document.querySelector('.shop__arrowL')
+nextButton.addEventListener('click', nextSlide)
+const prevButton = document.querySelector('.shop__arrowR')
+prevButton.addEventListener('click', prevSlide)
 
-renderProduct(catalogue.products);
+renderProduct(catalogue.products)
 
 function renderProduct(products) {
-    const productsContainer = document.querySelector('.shop__products');
-    productsContainer.innerHTML = '';
-    for (const product of products) {
-            productsContainer.innerHTML +=
-                `<div class="shop__product product">
+    const productsContainer = document.querySelector('.shop__products')
+    productsContainer.innerHTML = ''
+    for (let i = 0; i < products.length; i++) {
+        const content =
+            `<div class="shop__product product">
                 <div class="shop__product product">
-                    <a href="store-product.html"><img src="${product.images[0]}" alt="${product.name}"></img></a>
-                    <a href="store-product.html" class="product__name">${product.name}</a>
-                    <p class="product__price">${product.price.toFixed(2)}${product.currency}</p>
-                    <a class="button" href="#"><strong>Add to cart</strong></a>
+                <a href="store-product.html"><img src="${products[i].images[0]}" alt="${products[i].name}"></img></a>
+                <a href="store-product.html" class="product__name">${products[i].name}</a>
+                <p class="product__price">${products[i].price.toFixed(2)}${products[i].currency}</p>
+                <a class="button" href="#"><strong>Add to cart</strong></a>
                 </div>
             </div>`;
+        slides.push(content)
+    }
+    renderSlide()
+}
+
+function renderSlide() {
+    const productsContainer = document.querySelector('.shop__products')
+    productsContainer.innerHTML = ''
+    productsContainer.innerHTML += slides[currentSlide]
+    if (window.matchMedia('(min-height: 242px)').matches) {
+        const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1
+        productsContainer.innerHTML += slides[secondSlide]
+        if (window.matchMedia('(min-width: 1024px)').matches) {
+            const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1
+            productsContainer.innerHTML += slides[thirdSlide]
+        }
     }
 }
+
+function nextSlide() {
+    currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1
+    renderSlide()
+}
+
+
+function prevSlide() {
+    currentSlide = currentSlide - 1 < 0 ? slides.length - 1 : currentSlide - 1
+    renderSlide()
+}
+
+
+window.addEventListener('resize', renderProduct)
