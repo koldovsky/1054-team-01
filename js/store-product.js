496517f55cb7d76ad30ac463c1851ff4a4a512f3
Vyacheslav Anishchenko <avuua16@gmail.com>
2023-08-26 11:11:09 +0200

    store-product: unnecessary 'console.log' removed

--- js/store-product.js
+++ js/store-product.js
@@ -27,13 +27,11 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.storeproduct__form-uparrow').addEventListener('click', () => {
         quantity = Number(document.querySelector('.storeproduct__form-input').value) + 1
         document.querySelector('.storeproduct__form-input').value = quantity
-        console.log(quantity)
     })
 
     document.querySelector('.storeproduct__form-downarrow').addEventListener('click', () => {
         quantity = Math.max(1, document.querySelector('.storeproduct__form-input').value - 1)
         document.querySelector('.storeproduct__form-input').value = quantity
-        console.log(quantity)
     })
 
     document.addEventListener('partialsLoaded', () => {
