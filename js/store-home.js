5c76589d977fbda11fe8fce705093bd7f9cd27a0
Olesia Barannik <112662408+OlesiaBarannik@users.noreply.github.com>
2023-08-31 09:51:57 +0000

    added border on pageindex-container in store-home.js

--- js/store-home.js
+++ js/store-home.js
@@ -1,18 +1,20 @@
-import * as catalogue from './products-catalogue.js'
+import * as catalogue from './products-catalogue.js';
 
 document.addEventListener('partialsLoaded', () => {
-  const websiteLogo = document.querySelector('.websiteLogo')
-  websiteLogo.style.cursor = 'pointer'
-  websiteLogo.addEventListener('click', () => window.location.href = "index.html")
-})
+  const websiteLogo = document.querySelector('.websiteLogo');
+  websiteLogo.style.cursor = 'pointer';
+  websiteLogo.addEventListener('click', () => window.location.href = "index.html");
+});
 
 const productsArea = document.querySelector('.storehome__products-container');
 const pageButtonsContainer = document.querySelector('.storehome__pageindex-container');
 
+let selectedButton = null; // Keep track of the currently selected button
+
 function updatePageButtons() {
   const pageCountSelect = document.querySelector('.storehome__pagestyle-selector');
   const productsPerPage = parseInt(pageCountSelect.value);
-  
+
   const totalProducts = catalogue.products.length;
 
   pageButtonsContainer.innerHTML = '';
@@ -26,12 +28,21 @@ function updatePageButtons() {
     pageButtonsContainer.appendChild(button);
 
     button.addEventListener('click', () => {
+      if (selectedButton) {
+        selectedButton.classList.remove('selected'); // Remove border from previously selected button
+      }
+      selectedButton = button;
+      selectedButton.classList.add('selected'); // Add border to the clicked button
       showProductsByPage(i, productsPerPage);
     });
   }
 
-  // showAllProducts(1, productsPerPage);
-  showProductsByPage(1, productsPerPage);
+  // Initially select the first button and show its products
+  if (pageCount > 0) {
+    selectedButton = pageButtonsContainer.querySelector('.storehome__pageindex');
+    selectedButton.classList.add('selected');
+    showProductsByPage(1, productsPerPage);
+  }
 }
 
 function showProductsByPage(currentPage, productsPerPage) {
@@ -46,15 +57,11 @@ function showProductsByPage(currentPage, productsPerPage) {
       <div class="storehome__productcard">
           <a class="storehome__link" href="store-product.html?id=${item.code}">
           <figure class="storehome__figure">
-              <img class="storehome__img" src=${item.images[0]} alt="Bottle of ${
-      item.name
-    }" />
+              <img class="storehome__img" src=${item.images[0]} alt="Bottle of ${item.name}" />
               <figcaption class="storehome__caption">${item.name}</figcaption>
           </figure>
           </a>
-          <p class="storehome__price"><span>${item.price + ',00'}</span> ${
-      item.currency
-    }</p>
+          <p class="storehome__price"><span>${item.price + ',00'}</span> ${item.currency}</p>
           <button class="button storehome__button">Buy</button>
       </div>
     `;
