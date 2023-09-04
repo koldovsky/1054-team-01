5e9b44769d5d07191768190e68774938d59f1157
HolodLesia <45822753+HolodLesia@users.noreply.github.com>
2023-09-04 20:13:28 +0300

    Added changes to confirm.js file

--- js/confirm.js
+++ js/confirm.js
@@ -1,20 +1,10 @@
-// window.onload = function () {
-//     const modal = document.querySelector('.confirm'); 
-//     modal.classList.remove('hidden'); 
-// }
-  // Закрийте модальне вікно при натисканні на кнопку закриття або поза ним
-//   closeButton.onclick = function () {
-//     const closeButton = document.getElementsByClassName('confirm__window-button button')[0]; // Отримуємо кнопку закриття
-//     modal.style.display = 'none'; // Встановлюємо стиль "display" в "none" для закриття модального вікна
-// }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const modal = document.querySelector('confirm'); 
-//     modal.classList.add('hidden'); 
-// });
-document.querySelector('.confirm')
-.addEventListener("DOMContentLoaded", () => {
-    const modal = document.querySelector('.confirm'); 
-    modal.classList.remove('confirm'); 
-});
-
+window.onload = () => {
+  const modal = document.querySelector(".confirm");
+  modal.classList.remove("hidden-confirm");
+};
+document
+  .querySelector(".confirm__window-button .button") // Виправлено селектор
+  .addEventListener("click", () => {
+    const modal = document.querySelector(".confirm");
+    modal.classList.add("hidden-confirm");
+  });
