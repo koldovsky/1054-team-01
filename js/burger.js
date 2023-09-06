f9a9fa17b30849d4bd937e34fde3665002d57255
Jester <thettboy11@gmail.com>
2023-09-06 23:40:10 +0300

    scrollbar

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,8 @@
 const menuArrows = document.querySelectorAll('.burger');
+const body = document.body 
 menuArrows.forEach(burger => {
     burger.addEventListener('click', () => {
-        burger.classList.toggle('active');
+        burger.classList.toggle('active')
+        body.classList.toggle('disable__scrollbar');
     });
 });
