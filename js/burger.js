3e57e8a32e0dc60a8a427604978d5b5c744f5396
Jester <thettboy11@gmail.com>
2023-09-07 01:15:30 +0300

    fixed js

--- js/burger.js
+++ js/burger.js
@@ -1,8 +1,13 @@
-const menuArrows = document.querySelectorAll('.burger');
-const body = document.body 
-menuArrows.forEach(burger => {
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('active')
-        body.classList.toggle('disable__scrollbar');
-    });
+const burger = document.querySelector('.burger');
+const body = document.body;
+const links = document.querySelectorAll('.header__link');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  body.classList.toggle('disable__scrollbar');
+});
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    burger.classList.remove('active');
+    body.classList.remove('disable__scrollbar');
+  });
 });
